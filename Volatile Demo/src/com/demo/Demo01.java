608706b827a7388e8d8d3f9cf993985fc5c02902
rivulet <fullbenhouse@126.com>
2016-06-13 11:07:34 +0800

    添加包

---
+++ Volatile Demo/src/com/demo/Demo01.java
@@ -0,0 +1,42 @@
+package com.demo;
+
+
+/*
+ * 模式 #1：状态标志
+ */
+public class Demo01 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		final Worker worker = new Worker();
+		
+        for (int i = 0; i < 1000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                	worker.doWork();
+                }
+            }).start();
+        }
+        Thread.sleep(5000);
+        worker.shutdown();
+	}
+	
+}
+
+class Worker{
+	
+	volatile  boolean shutdownRequested;
+
+	public void shutdown() { shutdownRequested = true; }
+
+	public  void doWork() { 
+	    while (!shutdownRequested) { 
+	        
+	    	System.out.println("thread id : "+Thread.currentThread().getId() + " is running");
+	    }
+    	System.out.println("thread id : "+Thread.currentThread().getId() + " is stopped");
+
+	}
+	
+}
